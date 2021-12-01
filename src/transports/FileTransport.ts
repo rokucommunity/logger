55cb7f699e9e8ed42d35ebf4c98ba3e065d78740
Bronley Plumb <bronley@gmail.com>
2021-12-01 10:07:09 -0500

    Ensure the directory exists for file logging

--- src/transports/FileTransport.ts
+++ src/transports/FileTransport.ts
@@ -1,5 +1,6 @@
 import * as fsExtra from 'fs-extra';
 import { QueuedTransport } from './QueuedTransport';
+import * as path from 'path';
 
 export class FileTransport extends QueuedTransport {
     constructor(
@@ -13,6 +14,11 @@ export class FileTransport extends QueuedTransport {
         //if we have a logfile path, set the writer function which will flush the logs and enable future logging
         if (typeof logfilePath === 'string') {
             this.setWriter((message) => {
+                //make sure the parent directory exists
+                fsExtra.ensureDirSync(
+                    path.dirname(logfilePath)
+                );
+                //append the log entry to the file
                 fsExtra.appendFileSync(
                     logfilePath,
                     message.logger.formatMessage(message) + '\n'
